84bb2e73207b97837ea1ba74f6484e3c68e59a69
dhan <dhanesh@webyfy.com>
2023-06-15 16:30:47 +0530

    Initial commit
    load https://www.youtube.com/tv using custom user agent

---
+++ README.md
@@ -0,0 +1,7 @@
+# YouTube for TV desktop app
+
+
+## Install Dependencies
+```shell
+sudo apt install python3-pyside2.qtwebenginewidgets
+```
---
+++ src/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+from PySide2.QtCore import QUrl
+from PySide2.QtWidgets import QApplication,QMainWindow
+from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings, QWebEngineProfile, QWebEnginePage
+
+USER_AGENT = 'Roku/DVP-23.0 (23.0.0.99999-02)'
+
+def main():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    window.setWindowTitle("App")
+
+    webview = QWebEngineView()
+    webview.settings().setAttribute(QWebEngineSettings.ShowScrollBars, False)
+    profile = QWebEngineProfile.defaultProfile()
+    profile.setHttpUserAgent(USER_AGENT)
+    webpage = QWebEnginePage(profile, webview)
+    webview.setPage(webpage)
+    webview.load(QUrl("https://www.youtube.com/tv"))
+
+    window.setCentralWidget(webview)
+    window.show()
+
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
